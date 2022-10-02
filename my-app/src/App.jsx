a5e45ce203f07840b35e9916e7f0a2ea9c6a0ebc
hogan.lin <hogan.lin@titansofr.com.sg>
2022-10-02 10:52:33 +0800

    feat: star rating

--- my-app/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import EffectComponent from './EffectComponent'
-
-
-export default function App() {
-    return <EffectComponent></EffectComponent>
-}
--- my-app/src/components/StarRating.jsx
+++ my-app/src/components/StarRating.jsx
@@ -8,15 +8,15 @@ const Star = ({ selected = true, onSelect = f => f }) => (
 
 const createArray = length => [...Array(length)];
 
-export function StarRating({ totalStars = 5 }) {
+export default function StarRating({ totalStars = 5 }) {
     const [selectedStars, setSelectedStars] = useState(3)
     return <div>
-        {createArray(totalStars).map((n, i) => 
-<Star 
-    key={i} 
-    selected={selectedStars > i} 
-    onSelect={() => setSelectedStars(i + 1)} />
-)}
+        {createArray(totalStars).map((n, i) =>
+            <Star
+                key={i}
+                selected={selectedStars > i}
+                onSelect={() => setSelectedStars(i + 1)} />
+        )}
         <p>
             {selectedStars} of {totalStars} stars
         </p>
